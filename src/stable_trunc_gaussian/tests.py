2c053fda04942a207c70204423b9f9f0ac27e159
TheAeryan <e.ccaarlos@go.ugr.es>
2023-08-02 18:05:33 -0400

    Added function to tests.py for testing the icdf method

--- src/stable_trunc_gaussian/tests.py
+++ src/stable_trunc_gaussian/tests.py
@@ -100,6 +100,47 @@ log_prob_values = [
     [-999.45,-1130.8,1,-1000,-999]
 ]
 
+# Values for testing the icdf (and, thus, rsample)
+# In order: percentile, mu, sigma, a, b
+icdf_values = [
+    [0,0,1,-1,1],
+    [0.5,-1,1,-1,1],
+    [1,-10,1,-1,1],
+    [0.2,1,1,-1,1],
+    [0.01,10,1,-1,1],
+    [0.99,10,100,-1,1],
+    [0.3894,10,0.01,-1,1],
+
+    [0.1,2.5,1,2,3],
+    [0.2,2,1,2,3],
+    [0.3,3,1,2,3],
+    [0.4,0,1,2,3],
+    [0.5,-100,1,2,3],
+    [0.6,100,1,2,3],
+    [0.7,100,0.01,2,3],
+    [0.8,100,100,2,3],
+
+    [0.9,20,1,20,2000],
+    [1,2000,1,20,2000],
+    [0.9999,30,1,20,2000],
+    [0.0001,1990,1,20,2000],
+    [0.99999,10000,1,20,2000],
+    [0.00001,-10000,1,20,2000],
+    [0.4598,-10000,100,20,2000],
+
+    [0.1992,999.5,1,999,1000],
+    [0.1446,999,1,999,1000],
+    [0.78913,1000,1,999,1000],
+    [0.4113,3.5,1,999,1000],
+    [0.1617,1130.8,1,999,1000],
+
+    [0.0146,-999.5,1,-1000,-999],
+    [0.981616,-999,1,-1000,-999],
+    [0.16464,-1000,1,-1000,-999],
+    [0.89131,-3.5,1,-1000,-999],
+    [0.16566,-1130.8,1,-1000,-999]
+]
+
 # Returns the mean obtained with scipy and then the mean obtained with my code
 def calculate_mean(mu, sigma, a, b):
     m1 = TG(t(mu), t(sigma), t(a), t(b)).mean
@@ -125,6 +166,14 @@ def calculate_log_prob(x, mu, sigma, a, b):
 
     return p2, p1
 
+def calculate_icdf(percentile, mu, sigma, a, b):
+    icdf1 = TG(t(mu), t(sigma), t(a), t(b)).icdf(t(percentile))
+
+    a_, b_ = (a - mu) / sigma, (b - mu) / sigma
+    icdf2 = truncnorm.ppf(q=percentile, a=a_, b=b_, loc=mu, scale=sigma)
+
+    return icdf2, icdf1
+
 def check_mean_and_var():
     for mu, sigma, a, b in mean_and_val_values:
         t_mean, p_mean = calculate_mean(mu, sigma, a, b)
@@ -143,6 +192,14 @@ def check_log_prob():
         if not math.isclose(t_p, p_p, rel_tol=REL_TOL):
             print(f"Different log probs - Scipy:{t_p}, TN:{p_p} - x:{x}, mu:{mu}, sigma:{sigma}, a:{a}, b:{b}")
 
+def check_icdf():
+    for percentile, mu, sigma, a, b in icdf_values:
+        t_icdf, p_icdf = calculate_icdf(percentile, mu, sigma, a, b)
+
+        if not math.isclose(t_icdf, p_icdf, rel_tol=REL_TOL):
+            print(f"Different icdfs - Scipy:{t_icdf}, TN:{p_icdf} - percentile:{percentile}, mu:{mu}, sigma:{sigma}, a:{a}, b:{b}")
+
 if __name__ == '__main__':
-    check_mean_and_var()
-    check_log_prob()
+    #check_mean_and_var()
+    #check_log_prob()
+    check_icdf()
