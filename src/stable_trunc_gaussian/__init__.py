da366e765a44d1c8f3d4c8479fb03302c1cc089a
TheAeryan <ccaarlos@ugr.es>
2023-11-03 18:45:59 +0100

    Updated to version 1.3.8

--- src/stable_trunc_gaussian/__init__.py
+++ src/stable_trunc_gaussian/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
-__version__ = "1.3.7"
+__version__ = "1.3.8"
 
 from .parallel_trunc_gaussian import ParallelTruncatedGaussian as TruncatedGaussian
 # SequentialTruncatedGaussian is legacy code at this point, and lacks many methods such as rsample and icdf
