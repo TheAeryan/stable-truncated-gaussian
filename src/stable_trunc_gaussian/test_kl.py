Solved KL divergence bug for most cases. Nonetheless, there are still some approximation errors.

src/stable_trunc_gaussian/test_kl.py
@@ -2,7 +2,7 @@ from parallel_trunc_gaussian import ParallelTruncatedGaussian as TG
 import torch
 from torch import tensor as t
 from torch.distributions.kl import kl_divergence
-from scipy.stats import truncnorm, entropy
+from scipy.stats import truncnorm, entropy, norm
 import numpy as np
 
 def compare_var(mu, sigma, a, b):
@@ -12,6 +12,28 @@ def compare_var(mu, sigma, a, b):
     v2 = truncnorm.var(a=a_, b=b_, loc=mu, scale=sigma)
 
     return v2, v1
+
+def compare_mean(mu,sigma,a,b):
+    m1 = TG(t(mu), t(sigma), t(a), t(b)).mean
+
+    a_, b_ = (a - mu) / sigma, (b - mu) / sigma
+    m2 = truncnorm.mean(a=a_, b=b_, loc=mu, scale=sigma)
+
+    return m2, m1
+
+def scipy_log_Z(mu, sigma, a, b):
+    alpha = (a - mu) / sigma
+    beta = (b - mu) / sigma
+
+    Z = norm.cdf(beta) - norm.cdf(alpha)
+
+    return np.log(Z)
+
+def compare_log_Z(mu, sigma, a, b):
+    log_Z1 = TG(t(mu), t(sigma), t(a), t(b)).log_Z
+    log_Z2 = scipy_log_Z(mu, sigma, a, b)
+
+    return log_Z2, log_Z1
 
 def scipy_kl(d1_params, d2_params):
     mu1, sigma1, a1, b1 = d1_params
@@ -24,7 +46,7 @@ def scipy_kl(d1_params, d2_params):
     d1_scipy = truncnorm(a=a1_, b=b1_, loc=mu1, scale=sigma1)
     d2_scipy = truncnorm(a=a2_, b=b2_, loc=mu2, scale=sigma2)
 
-    n_points = int(1e5)
+    n_points = int(1e6)
     x = np.linspace(d2_scipy.ppf(1e-5), d2_scipy.ppf(1-1e-5), n_points)
 
     pdf1 = d1_scipy.pdf(x)
@@ -49,90 +71,29 @@ def compare_kl_and_var(params_list):
         kl_scipy, kl = compare_kl(params[0], params[1])
         v_scipy1, v1 = compare_var(params[0][0], params[0][1], params[0][2], params[0][3])
         v_scipy2, v2 = compare_var(params[1][0], params[1][1], params[1][2], params[1][3])
+        m_scipy1, m1 = compare_mean(params[0][0], params[0][1], params[0][2], params[0][3])
+        m_scipy2, m2 = compare_mean(params[1][0], params[1][1], params[1][2], params[1][3])
+        log_z_scipy1, log_z1 = compare_log_Z(params[0][0], params[0][1], params[0][2], params[0][3])
+        log_z_scipy2, log_z2 = compare_log_Z(params[1][0], params[1][1], params[1][2], params[1][3])
 
         print(f"\n------ Number {i} ------")
         print(f"Params: {params}")
         print(f"KL divergence: {kl_scipy} vs {kl}")
         print(f"Var 1: {v_scipy1} vs {v1}")
         print(f"Var 2: {v_scipy2} vs {v2}")
+        print(f"Mean 1: {m_scipy1} vs {m1}")
+        print(f"Mean 2: {m_scipy2} vs {m2}")
+        print(f"Log(Z) 1: {log_z_scipy1} vs {log_z1}")
+        print(f"Log(Z) 2: {log_z_scipy2} vs {log_z2}")
         print("------------------")
 
 # Example params to test kl and var
 # In order: first distribution, second distribution
 # Each distribution has mu,sigma,a,b
 # Important Note: the [a,b] interval of the first distribution must be a subset of the second distribution (a2 <= a1 <= b1 <= b2)
-
-# Generate params where both distributions always have mu=0 and sigma=1
-"""params_list=[
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.0764, 1.4142, 29.9000, 100000.1016)),
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (31, 1.1000, 29.9000, 100000.1016)),
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.05, 1.4, 29.9000, 100000.1016)),
-    ((3,1,-5,10000), (4,1.5,-5,10000)),
-    ((4,1.5,-5,10000), (3,1,-5,10000)),
-
-    ((0, 1, -1, 1), (0, 1, -1, 1)),
-    ((0, 1, -1, 1), (0, 1, -10, 10)),
-    ((0, 1, -1, 1), (0, 1, -1, 10)),
-    ((0, 1, -1, 1), (0, 1, -10, 1)),
-    ((0, 1, -10, 1), (0, 1, -10, 10)),
-    ((0, 1, -100, 0), (0, 1, -101, 32.45)),
-
-    ((0, 1, -1, 1), (0, 1, -1, 1e5)),
-    ((0, 1, -1, 1), (0, 1, -1e5, 1)),
-    ((0, 1, -1, 1), (0, 1, -1e6, 1e6)),
-
-    ((0, 1, 10, 20), (0, 1, 10, 20)),
-    ((0, 1, 10, 20), (0, 1, 10, 100)),
-    ((0, 1, 10, 20), (0, 1, -10, 20)),
-    ((0, 1, 10, 20), (0, 1, -10, 100)),
-
-    ((8, 1, 10, 20), (8, 1, 10, 20)),
-    ((8, 1, 10, 20), (8, 1, 10, 100)),
-    ((8, 1, 10, 20), (8, 1, -10, 20)),
-    ((8, 1, 10, 20), (8, 1, -10, 100)),
-]"""
-
 params_list=[
-    ((30, 1, 29, 100000), (31, 1, 29, 100000)),
-    ((30, 1, 29, 100000), (30, 2, 29, 100000)),
-    ((29, 1, 29, 100000), (30, 2, 29, 100000)),
-    ((29, 1, 29, 100000), (30, 2, 20, 100000)),
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.0764, 1.4142, 29.9000, 100000.1016)),
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (31, 1.1000, 29.9000, 100000.1016)),
-    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.05, 1.4, 29.9000, 100000.1016)),
-    ((3,1,-5,10000), (4,1.5,-5,10000)),
-    ((4,1.5,-5,10000), (3,1,-5,10000))
+    ((0, 0.1, -1, 1), (0, 5, -1, 1)),
 ]
 
 if __name__ == "__main__":
     compare_kl_and_var(params_list)
-
-
-# TODO
-"""
-- TG and scipy return almost the same variance
-- KL calculations are correct when mu=0, sigma=1
-- Normalizing a,b as in scipy before calculating KL with truncated does not work
-
----
-- KL calculations are correct when mu=0, sigma=1
-- They are also correct when both distribution d1, d2 have the same mu and sigma (even if mu != 0 and sigma != 1)
-
-
----- Changes between Entropy paper KL-divergence
-
-- a, b are numbers on the real line (not stds as in scipy)
-- Z_a_b(theta) = bigphi_theta(b) - bigphi_theta(a), where bigphi_theta is the cdf of a normal distribution with parameters theta
-    - bigphi_theta(x) -> CDF of a normal distribution with mu,sigma=theta up to x (x is not normalized, i.e., it is a instead of alpha)
-    - this is equivalent to the CDF of a standard normal distribution (mu=0,sigma=1) up to normalized x (i.e., alpha instead of a)
-
-- Z_a_b(m,s) = sqrt(2*pi)*s(bigphi_m_s(b) - bigphi_m_s(a))
-    - We need to multiply Z by sqrt(2*pi)*s!!
-
-> Formula analysis
-    - parameters m, s -> mu, sigma in my implementation (not normalized)
-    - moments (mean and variance) -> seem to be okay
-    - I think the issue is with log(Z2/Z1)
-        - multiply (bigphi(b) - bigphi(a)) by sqrt(2*pi)*s
-        - when calculating bigphi(x), divide x in erf(x) by sqrt2
-"""

src/stable_trunc_gaussian/tests.py
@@ -2,8 +2,10 @@
 # I compare the values obtained with my code against those returned by scipy.stats.truncnorm
 
 from parallel_trunc_gaussian import ParallelTruncatedGaussian as TG
-from scipy.stats import truncnorm
+from scipy.stats import truncnorm, entropy
+import numpy as np
 from torch import tensor as t
+from torch.distributions.kl import kl_divergence
 import math
 
 """
@@ -13,6 +15,15 @@ Current results:
       TG returns 4.100799560546875e-05 and scipy returns 0.00033332886587800203.
       Nonetheless, both values are very close to 0, so I don't think this is an issue
     - Log prob calculations are the same for scipy and TG
+
+    --- KL-divergence
+    It often performs well, but there are some errors:
+        >>> Scipy:1.8040149237108782, TN:14.501136779785156 - params:((0, 5, -1, 1), (0, 0.1, -1, 1))
+        - Scipy:0.6255919278246618, TN:0.7195351123809814 - params:((4, 1.5, -5, 10000), (3, 1, -5, 10000))
+        - Scipy:7.861164322976255e-05, TN:-4.57763671875e-05 - params:((100, 1, 10, 20), (99, 1, 10, 20))
+
+    It seems that these errors are not due to approximation errors in the variance calculation, mean calculation or log_Z calculation.
+    Maybe the formula used to calculate the KL is not correct?
 """
 
 # Two values are considered equal if their relative difference is less or equal than REL_TOL
@@ -141,6 +152,96 @@ icdf_values = [
     [0.16566,-1130.8,1,-1000,-999]
 ]
 
+# Values for testing the KL
+# The first element of each tuple is the first distribution, the second element is the second distribution
+# Each distribution has parameters corresponding to mu,sigma,a,b
+kl_values=[
+    ((30, 1, 29, 100000), (31, 1, 29, 100000)),
+    ((30, 1, 29, 100000), (30, 2, 29, 100000)),
+    ((29, 1, 29, 100000), (30, 2, 29, 100000)),
+    ((29, 1, 29, 100000), (30, 2, 20, 100000)),
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.0764, 1.4142, 29.9000, 100000.1016)),
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (31, 1.1000, 29.9000, 100000.1016)),
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.05, 1.4, 29.9000, 100000.1016)),
+    ((3,1,-5,10000), (4,1.5,-5,10000)),
+    ((4,1.5,-5,10000), (3,1,-5,10000)),
+
+    ((0, 1, -1, 1), (0, 1, -1, 1)),
+    ((0, 1, -1, 1), (0, 1, -10, 10)),
+    ((0, 1, -1, 1), (0, 1, -1, 10)),
+    ((0, 1, -1, 1), (0, 1, -10, 1)),
+    ((0, 1, -10, 1), (0, 1, -10, 10)),
+    ((0, 1, -100, 0), (0, 1, -101, 32.45)),
+
+    ((0, 1, -1, 1), (0, 1, -1, 1e5)),
+    ((0, 1, -1, 1), (0, 1, -1e5, 1)),
+    ((0, 1, -1, 1), (0, 1, -1e6, 1e6)),
+
+    ((0, 1, 10, 20), (0, 1, 10, 20)),
+    ((0, 1, 10, 20), (0, 1, 10, 100)),
+    ((0, 1, 10, 20), (0, 1, -10, 20)),
+    ((0, 1, 10, 20), (0, 1, -10, 100)),
+
+    ((8, 1, 10, 20), (8, 1, 10, 20)),
+    ((8, 1, 10, 20), (8, 1, 10, 100)),
+    ((8, 1, 10, 20), (8, 1, -10, 20)),
+    ((8, 1, 10, 20), (8, 1, -10, 100)),
+
+    # Varying means with the same sigma and interval
+    ((5, 1, 4, 100), (6, 1, 4, 100)),
+    ((10, 2, 9, 500), (12, 2, 9, 500)),
+    ((-5, 1, -6, 50), (-4, 1, -6, 50)),
+    
+    # Varying sigma with the same mean and interval
+    ((5, 1, 4, 100), (5, 2, 4, 100)),
+    ((10, 2, 9, 500), (10, 4, 9, 500)),
+    ((-5, 1, -6, 50), (-5, 3, -6, 50)),
+    
+    # Varying interval with the same mean and sigma
+    ((5, 1, 4, 10), (5, 1, 4, 50)),
+    ((10, 2, 9, 40), (10, 2, 9, 500)),
+    ((-5, 1, -20, -4), (-5, 1, -30, -4)),
+    
+    # Different means, sigmas, and intervals
+    ((10, 2, 9, 40), (-5, 3, -20, 50)),
+    
+    # Boundary cases
+    ((0, 1, -1, 1), (0, 2, -2, 2)),
+    
+    # Large vs small variance
+    ((0, 0.1, -1, 1), (0, 5, -1, 1)),
+    ((0, 5, -1, 1), (0, 0.1, -1, 1)),
+    
+    # Far away means
+    ((-1000, 5, -1005, -995), (1000, 5, -1010, 1005)),
+    ((-100, 2, -105, -95), (100, 2, -110, 105)),
+    
+    # Close means, far away intervals
+    ((5, 1, 4, 6), (5, 1, 4, 96)),
+    ((10, 2, 9, 11), (10, 2, 9, 91)),
+    ((-5, 1, -6, -4), (-5, 1, -56, -4)),
+    
+    # mu is below the interval [a, b]
+    ((-100, 1, 10, 20), (-99, 1, 10, 20)),
+    ((-50, 2, 0, 10), (-49, 2, 0, 10)),
+    ((-10, 3, 5, 15), (-9, 3, 5, 15)),
+    
+    # mu is above the interval [a, b]
+    ((100, 1, 10, 20), (99, 1, 10, 20)),
+    ((50, 2, 0, 10), (49, 2, 0, 10)),
+    ((10, 3, -15, -5), (9, 3, -15, -5)),
+    
+    # Large vs small variance with mu outside the interval
+    ((-100, 0.1, 10, 20), (-100, 5, 10, 20)),
+    ((50, 0.1, 0, 10), (50, 5, 0, 10)),
+    ((10, 0.1, -15, -5), (10, 5, -15, -5)),
+    
+    # Varying interval sizes with mu outside
+    ((-100, 1, 10, 11), (-100, 1, 10, 50)),
+    ((50, 2, 0, 1), (50, 2, 0, 20)),
+    ((10, 3, -5, -4), (10, 3, -5, 15)),
+]
+
 # Returns the mean obtained with scipy and then the mean obtained with my code
 def calculate_mean(mu, sigma, a, b):
     m1 = TG(t(mu), t(sigma), t(a), t(b)).mean
@@ -199,7 +300,47 @@ def check_icdf():
         if not math.isclose(t_icdf, p_icdf, rel_tol=REL_TOL):
             print(f"Different icdfs - Scipy:{t_icdf}, TN:{p_icdf} - percentile:{percentile}, mu:{mu}, sigma:{sigma}, a:{a}, b:{b}")
 
+def scipy_kl(d1_params, d2_params):
+    mu1, sigma1, a1, b1 = d1_params
+    mu2, sigma2, a2, b2 = d2_params
+
+    # Normalize parameters
+    a1_, b1_ = (a1 - mu1) / sigma1, (b1 - mu1) / sigma1
+    a2_, b2_ = (a2 - mu2) / sigma2, (b2 - mu2) / sigma2
+
+    d1_scipy = truncnorm(a=a1_, b=b1_, loc=mu1, scale=sigma1)
+    d2_scipy = truncnorm(a=a2_, b=b2_, loc=mu2, scale=sigma2)
+
+    n_points = int(1e5)
+    x = np.linspace(d2_scipy.ppf(1e-5), d2_scipy.ppf(1-1e-5), n_points)
+
+    pdf1 = d1_scipy.pdf(x)
+    pdf2 = d2_scipy.pdf(x)
+
+    return entropy(pdf1, pdf2)
+
+def compare_kl(d1_params, d2_params):
+    mu1, sigma1, a1, b1 = d1_params
+    mu2, sigma2, a2, b2 = d2_params
+
+    d1 = TG(t(mu1), t(sigma1), t(a1), t(b1))
+    d2 = TG(t(mu2), t(sigma2), t(a2), t(b2))
+
+    kl = kl_divergence(d1, d2)
+    kl_scipy = scipy_kl(d1_params, d2_params)
+
+    return kl_scipy, kl
+
+def check_kl():
+    for params in kl_values:
+        kl_scipy, kl = compare_kl(params[0], params[1])
+
+        if not math.isclose(kl_scipy, kl, rel_tol=REL_TOL):
+            print(f"Different KLs - Scipy:{kl_scipy}, TN:{kl} - params:{params}")
+
+
 if __name__ == '__main__':
     #check_mean_and_var()
     #check_log_prob()
-    check_icdf()
+    #check_icdf()
+    check_kl()