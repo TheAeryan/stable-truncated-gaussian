Solving bug KL divergence #3

src/stable_trunc_gaussian/test_kl.py
@@ -5,7 +5,7 @@ from torch.distributions.kl import kl_divergence
 from scipy.stats import truncnorm, entropy
 import numpy as np
 
-def calculate_var(mu, sigma, a, b):
+def compare_var(mu, sigma, a, b):
     v1 = TG(t(mu), t(sigma), t(a), t(b)).variance
 
     a_, b_ = (a - mu) / sigma, (b - mu) / sigma
@@ -13,52 +13,104 @@ def calculate_var(mu, sigma, a, b):
 
     return v2, v1
 
-def kl_approx(d1, d2):
-    mu1, sigma1, a1, b1 = d1.mu, d1.sigma, d1.a, d1.b
-    mu2, sigma2, a2, b2 = d2.mu, d2.sigma, d2.a, d2.b
+def scipy_kl(d1_params, d2_params):
+    mu1, sigma1, a1, b1 = d1_params
+    mu2, sigma2, a2, b2 = d2_params
 
+    # Normalize parameters
     a1_, b1_ = (a1 - mu1) / sigma1, (b1 - mu1) / sigma1
     a2_, b2_ = (a2 - mu2) / sigma2, (b2 - mu2) / sigma2
 
     d1_scipy = truncnorm(a=a1_, b=b1_, loc=mu1, scale=sigma1)
     d2_scipy = truncnorm(a=a2_, b=b2_, loc=mu2, scale=sigma2)
 
-    x = np.linspace(d2.a, d2.b, 100000)
+    n_points = int(1e5)
+    x = np.linspace(d2_scipy.ppf(1e-5), d2_scipy.ppf(1-1e-5), n_points)
 
     pdf1 = d1_scipy.pdf(x)
     pdf2 = d2_scipy.pdf(x)
+
     return entropy(pdf1, pdf2)
 
-d1 = TG(t(30.0764),t(1.4142),t(29.9000),t(100000.1016))
-d2 = TG(t(31), t(1.1000), t(29.9000), t(100000.1016))
-d3 = TG(t(30.05), t(1.4), t(29.9000), t(100000.1016))
+def compare_kl(d1_params, d2_params):
+    mu1, sigma1, a1, b1 = d1_params
+    mu2, sigma2, a2, b2 = d2_params
 
-d4 = TG(t(3),t(1),t(-5),t(10000))
-d5 = TG(t(4),t(1.5),t(-5),t(10000))
+    d1 = TG(t(mu1), t(sigma1), t(a1), t(b1))
+    d2 = TG(t(mu2), t(sigma2), t(a2), t(b2))
 
-print("KL(d1|d2)", kl_divergence(d1,d2))
-print("<Scipy> KL(d1|d2)", kl_approx(d1,d2))
+    kl = kl_divergence(d1, d2)
+    kl_scipy = scipy_kl(d1_params, d2_params)
 
+    return kl_scipy, kl
 
-print("KL(d2|d1)", kl_divergence(d2,d1))
-print("KL(d1|d1)", kl_divergence(d1,d1))
-print("KL(d2|d2)", kl_divergence(d2,d2))
-print("KL(d1|d3)", kl_divergence(d1,d3))
+def compare_kl_and_var(params_list):
+    for i, params in enumerate(params_list):
+        kl_scipy, kl = compare_kl(params[0], params[1])
+        v_scipy1, v1 = compare_var(params[0][0], params[0][1], params[0][2], params[0][3])
+        v_scipy2, v2 = compare_var(params[1][0], params[1][1], params[1][2], params[1][3])
 
-print("KL(d4|d5)", kl_divergence(d4,d5))
-print("KL(d5|d4)", kl_divergence(d5,d4))
+        print(f"\n------ Number {i} ------")
+        print(f"Params: {params}")
+        print(f"KL divergence: {kl_scipy} vs {kl}")
+        print(f"Var 1: {v_scipy1} vs {v1}")
+        print(f"Var 2: {v_scipy2} vs {v2}")
+        print("------------------")
 
-print(" ---- Variance ---- ")
+# Example params to test kl and var
+# In order: first distribution, second distribution
+# Each distribution has mu,sigma,a,b
+# Important Note: the [a,b] interval of the first distribution must be a subset of the second distribution (a2 <= a1 <= b1 <= b2)
 
-vars1 = calculate_var(30.0764,1.4142,29.9000,100000.1016)
-vars2 = calculate_var(31,1.1000,29.9000,100000.1016)
+# Generate params where both distributions always have mu=0 and sigma=1
+"""params_list=[
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.0764, 1.4142, 29.9000, 100000.1016)),
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (31, 1.1000, 29.9000, 100000.1016)),
+    ((30.0764, 1.4142, 29.9000, 100000.1016), (30.05, 1.4, 29.9000, 100000.1016)),
+    ((3,1,-5,10000), (4,1.5,-5,10000)),
+    ((4,1.5,-5,10000), (3,1,-5,10000)),
 
-print(torch.abs(vars1[0]-vars1[1]))
-print(torch.abs(vars2[0]-vars2[1]))
+    ((0, 1, -1, 1), (0, 1, -1, 1)),
+    ((0, 1, -1, 1), (0, 1, -10, 10)),
+    ((0, 1, -1, 1), (0, 1, -1, 10)),
+    ((0, 1, -1, 1), (0, 1, -10, 1)),
+    ((0, 1, -10, 1), (0, 1, -10, 10)),
+    ((0, 1, -100, 0), (0, 1, -101, 32.45)),
+
+    ((0, 1, -1, 1), (0, 1, -1, 1e5)),
+    ((0, 1, -1, 1), (0, 1, -1e5, 1)),
+    ((0, 1, -1, 1), (0, 1, -1e6, 1e6)),
+
+    ((0, 1, 10, 20), (0, 1, 10, 20)),
+    ((0, 1, 10, 20), (0, 1, 10, 100)),
+    ((0, 1, 10, 20), (0, 1, -10, 20)),
+    ((0, 1, 10, 20), (0, 1, -10, 100)),
+]"""
+
+params_list=[
+    ((0, 1, -1, 1), (0, 1, -1, 1)),
+    ((0, 1, -1, 1), (0, 1, -10, 10)),
+    ((0, 1, -1, 1), (0, 1, -1, 10)),
+    ((0, 1, -1, 1), (0, 1, -10, 1)),
+
+    ((0.5,2,-1,1), (0.5,2,-1,1)),
+    ((0.5,2,-1,1), (0.5,2,-5,1)),
+    ((0.5,2,-1,1), (0.5,2,-1,5)),
+
+    ((1,2,-10,10), (0.5,2,-10,10)),
+    ((2,4,-10,10), (0.5,4,-10,10)),
+    ((3,8,-10,10), (0.5,8,-10,10)),
+]
+
+if __name__ == "__main__":
+    compare_kl_and_var(params_list)
+
 
 # TODO
 """
-- Compare results with scipy's kl divergence
-- See if the KL divergence is wrong due to variance calculations
+- TG and scipy return almost the same variance
+- KL calculations are correct when mu=0, sigma=1
+- Normalizing a,b as in scipy before calculating KL with truncated does not work
+
 
 """